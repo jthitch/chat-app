cce83e593ac69cc590a40153cd849d55622366e5
James H <jimhitchman@hotmail.com>
2024-07-04 16:02:11 +0100

    initalialised firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app'
+
+const config = {
+    apiKey: "AIzaSyAYaJ_rO924_5ckO1cQEiryc42JsN_M3Gc",
+    authDomain: "chat-app-bb6cb.firebaseapp.com",
+    databaseURL: "https://chat-app-bb6cb-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "chat-app-bb6cb",
+    storageBucket: "chat-app-bb6cb.appspot.com",
+    messagingSenderId: "971925548843",
+    appId: "1:971925548843:web:ec26a449853128b0979f6e",
+    measurementId: "G-GW5VYLELZ8"
+  };
+
+const app =firebase.initializeApp(config);
