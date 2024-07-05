4fdf3b939c757717519c6bd7d68d8e0bc16ad425
James H <jimhitchman@hotmail.com>
2024-07-05 17:41:12 +0100

    added public and private routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+);
