2a85c4e86ad8dad5c66343656fabd1ddab28d95a
James H <jimhitchman@hotmail.com>
2024-08-21 17:01:38 +0100

    created rooms context

--- src/misc/helpers.js
+++ src/misc/helpers.js
@@ -8,3 +8,9 @@ export function getNameInitals(name){
     return splitName[0][0];
  
 }
+
+export function transformToArrWithId(snapVal){
+    return snapVal ? Object.keys(snapVal).map(roomId => {
+        return{...snapVal[roomId], id: roomId};
+    }) : []
+}
