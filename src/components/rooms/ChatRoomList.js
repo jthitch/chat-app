d616daad087db265a14e073e3547c811c4ddbf5a
James H <jimhitchman@hotmail.com>
2024-08-22 16:54:58 +0100

    displayed chatrooms

--- src/components/rooms/ChatRoomList.js
+++ src/components/rooms/ChatRoomList.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import RoomItem from './RoomItem'
-import { Nav } from 'rsuite'
+import { Loader, Nav } from 'rsuite'
+import { useRooms } from '../../context/rooms.context'
+import { Link } from 'react-router-dom'
+import { useLocation } from 'react-router-dom/cjs/react-router-dom.min'
 
 const ChatRoomList = ({aboveElHeight}) => {
-  return  <Nav appearance='subtle' vertical reversed className='overflow-y-scroll custom-scroll' style={{height: `calc(100% - ${aboveElHeight}px)`}}>
-    <Nav.Item>
-        <RoomItem />
-    </Nav.Item>
+
+    const rooms = useRooms();
+    const location = useLocation();
+
+
+  return  <Nav appearance='subtle' vertical reversed className='overflow-y-scroll custom-scroll' style={{height: `calc(100% - ${aboveElHeight}px)`}} activeKey={location.pathname}>
+    {!rooms && (<Loader center vertical content="Loading" speed="slow" size="md" />)}
+    {rooms && rooms.length > 0 && rooms.map(room => 
+    <Nav.Item componentClass={Link} to={`/chat/${room.id}`} key={room.id} eventKey={`/chat/${room.id}`}>
+        <RoomItem room={room}/>
+    </Nav.Item>)}
+
   </Nav>
 }
 
